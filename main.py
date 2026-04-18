a11803767798b1eea59c4b40e1b877583f2f1b78
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-18 15:36:17 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,13 @@
+def tub_ko_paytuvchilar(n):
+    tub_ko_paytuvchilar = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    return [x for x in tub_ko_paytuvchilar if n % x == 0]
+
+print(tub_ko_paytuvchilar(100))
+```
+
+```python
+def tub_ko_paytuvchilar(n):
+    tub_ko_paytuvchilar = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    return [x for x in tub_ko_paytuvchilar if n % x == 0]
+
+print(tub_ko_paytuvchilar(100))
